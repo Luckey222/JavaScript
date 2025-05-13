4b27f6a514bd44f9aea2ec25ece2bfb5100b64e0
Luckey222 <Laxumanbhattarai222@gmail.com>
2025-05-13 09:57:36 +0545

    Initial commit for new-js branch

---
+++ basic/practice.js
@@ -0,0 +1,44 @@
+//  const name= new String("laxman")
+// console.log(Name.split('la'));
+ 
+
+/*
+const gameName = new String('laxman');
+console.log(gameName.length); 
+
+
+const newstring=gameName.substring(2,6)
+console.log(newstring);
+
+
+const anotherString=newstring.slice(-8,4)
+console.log(anotherString);
+
+*/
+// const hundreds=5000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+
+
+// +++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++++++
+min=10
+max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);      
+/* formula to calculate random numbr according to min and maxn value passed by th usr or user requirdment  */ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ basic/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+ const fruits = ["apple", "banana", "cherry", "date"];
+// const result = fruits.slice(1, 3);
+// console.log(result);  
